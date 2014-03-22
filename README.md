3426553eb2cb20af44224098f0d3ce06c48aac8d
Mollie Taylor <mollie.taylor@gmail.com>
2014-03-21 20:02:55 -0400

    Adds flashing and sound to tomatoes.sh

--- README.md
+++ README.md
@@ -16,8 +16,3 @@ tomatoes.sh is a timer where pomodoros start on the hour and half hour. This all
 Pomodoro
 --------
 The pomodoro.sh begins the pomodoro when you run the script. There are variables that control the length of pomodoros and breaks, so you can alter these if you want.
-
-To Do
------
-tomatoes.sh: have it blink a few times at the beginning of each tomato and break
-
--- tomatoes.sh
+++ tomatoes.sh
@@ -7,14 +7,35 @@ tomato_color="255,0,0"
 
 break_color="0,255,0"
 
+# initial color:
+minute=$(date +"%M")
+minute=`echo $minute|sed 's/^0*//'`
+if [[ "$minute" -lt 25 || "$minute" -ge 30 && "$minute" -lt 55 ]]
+then
+	$blink_path --rgb $tomato_color > /dev/null 2>&1
+else
+	$blink_path --rgb $break_color > /dev/null 2>&1
+fi
+
+# change colors and beep:
 while true; do
 	minute=$(date +"%M")
 	minute=`echo $minute|sed 's/^0*//'`
-	if [[ "$minute" -lt 25 || "$minute" -ge 30 && "$minute" -lt 55 ]]
+	second=$(date +"%S")
+	second=`echo $second|sed 's/^0*//'`
+	if [[ "$second" -eq 0 ]]
 	then
-		$blink_path --rgb $tomato_color > /dev/null 2>&1
-	else
-		$blink_path --rgb $break_color > /dev/null 2>&1
+		if [[ "$minute" -eq 0 || "$minute" -eq 30 ]]
+		then
+			paplay /usr/share/sounds/KDE-Im-Highlight-Msg.ogg # beep
+			$blink_path --rgb $tomato_color --blink 4 > /dev/null 2>&1
+			$blink_path --rgb $tomato_color > /dev/null 2>&1
+		elif [[ "$minute" -eq 25 || "$minute" -eq 55 ]]
+		then
+			paplay /usr/share/sounds/KDE-Im-Highlight-Msg.ogg # beep
+			$blink_path --rgb $break_color --blink 4 > /dev/null 2>&1
+			$blink_path --rgb $break_color > /dev/null 2>&1
+		fi
 	fi
-	sleep 5 # loop only runs every 5 seconds
+	sleep 5 # loop runs every 5 seconds
 done
